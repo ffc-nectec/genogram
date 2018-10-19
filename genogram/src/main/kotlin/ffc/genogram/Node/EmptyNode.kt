6ea161d32e49f192fa853c554aa51c73403cb5e7
Nat Su <natthanicha.suriyamongkol@nectec.or.th>
2018-10-19 22:23:22 +0700

    Added "EmptyNode"

---
+++ genogram/src/main/kotlin/ffc/genogram/Node/EmptyNode.kt
@@ -0,0 +1,27 @@
+package ffc.genogram.Node
+
+import ffc.genogram.FamilyTreeDrawer
+import ffc.genogram.RelationshipLine.RelationshipLabel
+
+class EmptyNode: Node() {
+
+    lateinit var nodeString: String
+
+    override fun drawNode(relationLabel: RelationshipLabel?, siblings: Boolean): FamilyTreeDrawer? {
+        return null
+    }
+
+    override fun getArea(): Double {
+        return nodeSize + nodeBorderSize
+    }
+
+    fun drawEmptyNode(): String {
+        nodeString = StringBuilder().toString()
+
+        for (i in 0 until Node.nodeSize.toInt() + nodeBorderSize.toInt()) {
+            nodeString += " "
+        }
+
+        return nodeString
+    }
+}
