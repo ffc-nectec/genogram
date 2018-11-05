2704b3450511b46a53445c59c7cfd6945801f0d3
Piruin Panichphol <piruin.p@gmail.com>
2018-11-05 20:22:19 +0700

    create bloodFamily from members

---
+++ genogram/src/test/kotlin/ffc/genogram/FamilyTest.kt
@@ -0,0 +1,32 @@
+package ffc.genogram
+
+import org.amshove.kluent.shouldContainSame
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.Parameterized
+
+@RunWith(Parameterized::class)
+class FamilyTest(private val resource: String, private val expectBloodFamily: List<Int>) {
+
+    companion object {
+        @JvmStatic
+        @Parameterized.Parameters
+        fun data(): Collection<Array<Any>> {
+            return listOf(
+                    arrayOf("2ndGen/spouses/family-4-spouses-1.json",
+                            listOf(0, 1, 2, 3, 4)),
+                    arrayOf("3rdGen/family-5-children-3rd-gen-2.json",
+                            listOf(0, 1, 2, 3, 4, 5, 6, 7, 8, 9)),
+                    arrayOf("3rdGen/family-3-children-3rd-gen-6.json",
+                            listOf(0, 1, 2, 3, 4, 5, 6, 7))
+            )
+        }
+    }
+
+    @Test
+    fun bloodFamily() {
+        val family = Family(1, "Smiths", getResourceAs<Family>(resource).members)
+
+        family.bloodFamily!! shouldContainSame expectBloodFamily
+    }
+}
